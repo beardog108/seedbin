8070bc5f31b5e6d165e57164a688ec69d715a8e5
Kevin Froman <kevinfroman12@gmail.com>
2017-05-24 14:12:20 -0500

    updated license

--- paste.php
+++ paste.php
@@ -1,7 +1,29 @@
 <?php
+/*
+SeedBin: a distributed pastebin with ipfs and webtorrent
+    Copyright (C) 2017 Kevin Forman (beardog108) https://ChaosWebs.net/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public
+License
+    along with this program.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+/*
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
+*/
 header('access-control-allow-origin: *');
 
 $chk = curl_init('http://127.0.0.1:5001');
@@ -15,16 +37,12 @@ if (! 200 == $retcode) {
     die('data: Server is down :(, will use WebTorrent');
 }
 
-
 include('easy-ipfs.php');
 $ipfs = new IPFS();
 
 if (isset($_GET['cat'])){
   die($ipfs->cat($_GET['cat']));
 }
-
-
-//file_put_contents('yolo.txt', 'test');
 
 $title = bin2hex(openssl_random_pseudo_bytes(3)) . time() . '.txt';
 $title = (string)$title;
